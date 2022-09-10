465579c9e1045d470af895ae8c08302b2e7263bb
cArlitXs <carlitosalvarezlinares@gmail.com>
2022-09-10 15:51:37 +0200

    Add error message

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -5,20 +5,28 @@ import './container.css';
 
 export const Container = () => {
   const [photos, setPhotos] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       await photosApi
         .getPhotos({ page: 4 })
         .then(({ photos }) => setPhotos(photos))
-        .catch((err) => console.error(err));
+        .catch((err) => {
+          console.error(err);
+          setError(err.message);
+        });
     };
     fetchData();
   }, [photos]);
 
   return (
     <div className="container">
-      {photos.length > 0 ? photos.map((p, i) => <Picture key={i} src={p.previewUrl} />) : <p>No photos</p>}
+      {photos.length > 0 ? (
+        photos.map((p, i) => <Picture key={i} src={p.previewUrl} />)
+      ) : (
+        <h2 className="message">{error.length > 0 ? `Error: ${error}` : 'No photos'}</h2>
+      )}
     </div>
   );
 };
